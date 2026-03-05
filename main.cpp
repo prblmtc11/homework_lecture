a78c97a00f6df4621446b5390d69dbb7b4178d22
prblmtc11 <prblmtc11@gmail.com>
2026-03-05 15:17:11 +0300

    array ToList

--- main.cpp
+++ main.cpp
@@ -5,4 +5,54 @@ struct BiList {
     T val;
     BiList<T>* next;
     BiList<T>* prev;
+};
+
+template<class T>
+BiList<T>* createNode(const T& value) {
+    BiList<T>* node = new BiList<T>;
+    node->val  = value;
+    node->next = nullptr;
+    node->prev = nullptr;
+    return node;
+}
+
+template<class T>
+void pushBack(BiList<T>*& head, BiList<T>*& tail, const T& value) {
+    BiList<T>* node = createNode(value);
+    if (tail == nullptr) {
+        head = tail = node;
+    } else {
+        node->prev = tail;
+        tail->next = node;
+        tail = node;
+    }
+}
+
+template<class T>
+void freeList(BiList<T>*& head, BiList<T>*& tail) {
+    BiList<T>* cur = head;
+    while (cur != nullptr) {
+        BiList<T>* next = cur->next;
+        delete cur;
+        cur = next;
+    }
+    head = tail = nullptr;
+}
+
+template<class T>
+void printForward(BiList<T>* head) {
+    for (BiList<T>* cur = head; cur != nullptr; cur = cur->next) {
+        std::cout << cur->val;
+        if (cur->next) std::cout << " <-> ";
+    }
+    std::cout << "\n";
+}
+
+template<class T>
+void printBackward(BiList<T>* tail) {
+    for (BiList<T>* cur = tail; cur != nullptr; cur = cur->prev) {
+        std::cout << cur->val;
+        if (cur->prev) std::cout << " <-> ";
+    }
+    std::cout << "\n";
 }
